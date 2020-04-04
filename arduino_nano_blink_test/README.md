977de580c99c1203d401ce2155b4934d832631b4
Julian Straub <jstraub@csail.mit.edu>
2020-04-04 12:34:48 -0700

    link

--- arduino_nano_blink_test/README.md
+++ arduino_nano_blink_test/README.md
@@ -1,1 +1,5 @@
+more details at
+http://julianstraub.com/posts/programming_arduino_in_c/
+
+inspired by
 https://blog.podkalicki.com/how-to-compile-and-burn-the-code-to-avr-chip-on-linuxmacosxwindows/
--- arduino_nano_blink_test/main.c
+++ arduino_nano_blink_test/main.c
@@ -2,12 +2,14 @@
 #include <util/delay.h>
 
 int main(void) {
+  // set LED pin PB5 and PB4 to output
+  DDRB |= (1<<PB5) | (1<<PB5);
 
-  DDRB |= (1<<PB5) | (1<<PB4);
-
-  while(42) { 
+  while(true) {
+    // toggle the LED pin PB5 and PB4 and hence the LED
     PORTB ^= (1<<PB5);
     PORTB ^= (1<<PB4);
+    // use build in delay function to delay for 100ms
     _delay_ms(100);
   }
 }
