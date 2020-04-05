1d284fc206751bde1eb3c60579211dd4046920e4
Julian Straub <jstraub@csail.mit.edu>
2020-04-05 15:11:07 -0700

    plot labels

--- arduino_nano_voltage_logger/plot.py
+++ arduino_nano_voltage_logger/plot.py
@@ -4,10 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.loadtxt("./liIonDischarge.csv")
+t = np.arange(x.shape[0])*1000./1024.
 xSmooth = np.convolve(x[:,1], np.ones(60)/60., mode="valid")
 
 plt.figure()
-plt.plot(x[:,1], label="raw")
-plt.plot(xSmooth, label="smooth")
+plt.plot(x[:,1], "b", alpha=0.5, label="raw")
+plt.plot(xSmooth, "r", label="smooth")
+plt.xlabel("time [s]")
+plt.ylabel("voltage [V]")
 plt.legend()
 plt.show()
