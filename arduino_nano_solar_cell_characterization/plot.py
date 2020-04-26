20a036c62a381444ead067d0ad8d51ccf9c8598a
Julian Straub <jstraub@csail.mit.edu>
2020-04-26 10:04:49 -0700

    nocer plotting

--- arduino_nano_solar_cell_characterization/plot.py
+++ arduino_nano_solar_cell_characterization/plot.py
@@ -3,6 +3,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
+f = "./test.csv"
 f = "./indoorBrightNotDirectLight.csv"
 f = "./indoorInLight.csv"
 
@@ -11,18 +12,20 @@ i, u = x[:,0], x[:,1]
 p = i*u
 
 for T in range(4, 30, 3):
-    print("T={}, R_0 {}".format(T, -(u[0]-u[T]) / (i[0]-i[T])))
+    print("T={},\tR_series {}".format(T, -(u[0]-u[T]) / (i[0]-i[T])))
 for T in range(4, 30, 3):
-    print("T={}, R_e {}".format(T, -(u[-1]-u[-T]) / (i[-1]-i[-T])))
+    print("T={},\tR_shunt  {}".format(T, -(u[-1]-u[-T]) / (i[-1]-i[-T])))
 
 
 fig, ax1 = plt.subplots()
 
 ax2 = ax1.twinx()
-ax1.plot(u, i*1e3, 'g-')
-ax2.plot(u, p*1e3, 'b-')
+ax1.plot(u, i*1e3, "b-", lw=2, label="current")
+ax2.plot(u, p*1e3, "g-", lw=2, label="power")
 
 ax1.set_xlabel("voltage [V]")
 ax1.set_ylabel("current [mA]")
 ax2.set_ylabel("power [mW]")
+ax1.legend(loc="upper left")
+ax2.legend(loc="upper right")
 plt.show()
